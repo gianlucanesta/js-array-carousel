0e43d4c021ae55669a27abda9767b51e962b0bcf
gianlucanesta <gianluca.nesta@gmail.com>
2021-11-18 15:53:26 +0100

    Scaffolding

---
+++ milestone2/js/main.js
@@ -0,0 +1,4 @@
+// MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo le immagini e i testi delle immagini dinamicamente servendoci dell'array 
+// fornito e un semplice ciclo for che concatena un template literal. Al termine di questa fase ci ritroveremo con lo stesso slider, 
+// ma costruito dinamicamente attraverso JavaScript.
---
+++ milestone3/js/main.js
@@ -0,0 +1,9 @@
+// MILESTONE 3
+// Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventa visibile in formato grande a sinistra 
+// e nel suo angolo in basso a destra dovranno comparire relativi:
+//  - titolo e
+//  - testo.
+// Allo stesso tempo nelle miniature l'immagine attiva dovrà apparire in evidenza rispetto alle altre.
+// BONUS:
+// Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, 
+// la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
